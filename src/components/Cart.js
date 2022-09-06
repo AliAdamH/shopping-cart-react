3146bc2c465b2a4de1b05da2f26aee79feb6517d
Ali Hassaine <ali.hs@outlook.fr>
2022-09-06 04:09:01 +0000

    Add base components

---
+++ src/components/Cart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cart = () => {
+  return (
+    <>
+      <h1> This is the shopping cart component</h1>
+    </>
+  );
+};
+
+export default Cart;
---
+++ src/components/Product.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Product = () => {
+  return (
+    <>
+      <h2>This should be the product card</h2>
+    </>
+  );
+};
+
+export default Product;
