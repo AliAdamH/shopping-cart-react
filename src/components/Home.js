92410b231d0775a76dc683be6e3b4fd78cf7f716
Ali Hassaine <ali.hs@outlook.fr>
2022-09-07 01:16:22 +0000

    Setup home hero

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 const Home = () => {
   return (
-    <div className="container">
-      <h1>This is the homepage</h1>
+    <div className="hero">
+      <div className="home">
+        <h1 className="home__title">
+          Over 300 unique and rare
+          <span className="home__title__fancy"> Flowers</span> from all over the
+          globe
+        </h1>
+        <Link className="home__link" to="/product">
+          Explore
+        </Link>
+      </div>
     </div>
   );
 };
